c41a2ddbc4264453513f43b12fef97a98cb09185
Mitul Kumar <mitulkr9110@gmail.com>
2026-09-12 12:23:40 +0530

    Initial project setup

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    success: true,
+    message: "Mini Operations ERP API is running"
+  });
+});
+
+export default app;
